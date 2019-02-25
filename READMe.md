5a67424ce996065d08c537b94010de01231154bb
Anxium <axel.avx@gmail.com>
2019-02-25 12:04:59 +0100

    init axel

---
+++ READMe.md
@@ -0,0 +1,19 @@
+# Binding of Odul
+Jeu effectué en nodeJS avec phaser3 pour la GameJam organisée par Ludovic Patho à BeCode - Charleroi (CRL-Turing-2.6)
+#### The team
+Axel Avaux - Project Manager (Fullstack Dev)
+
+Justine Crenier - Project Manager Assistant (Graphiste & Dev)
+
+Ruben Pereira C - Graphiste (Couteau suisse Portugais)
+
+Thomas Bailleux - Dev
+
+Valentin Beaufays - Dev
+
+Jérémy Creloa - Dev
+
+## Quand on a récup le projet
+1. npm install 
+ - Devrait installer toutes les dépendances requises pour le projet
+2. npm start
--- assets/client/client.js
+++ assets/client/client.js
@@ -21,14 +21,80 @@ const game = new Phaser.Game(config)
 function preload ()
 {
 
+    this.load.spritesheet('dude',
+        'img/dude.png',
+        { frameWidth: 60, frameHeight: 74 }
+    )
+
 }
+
+let player;
 
 function create ()
 {
+
+    player = this.physics.add.sprite(100, 450, 'dude')
+
+    player.setCollideWorldBounds(true) 
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    }) 
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    }) 
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    }) 
 
 }
 
 function update ()
 {
+
+    // Mouvement effectué grâce au clavier
+    cursors = this.input.keyboard.createCursorKeys() 
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160) 
+
+        player.anims.play('left', true) 
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160) 
+
+        player.anims.play('right', true) 
+    }
+    else 
+    {
+        player.setVelocityX(0)
+
+        player.anims.play('turn') 
+    }
+
+    if (cursors.up.isDown)
+    {
+        player.setVelocityY(-160) 
+    }
+    else if (cursors.down.isDown)
+    {
+        player.setVelocityY(160)
+    }
+    else 
+    {
+        player.setVelocityY(0)
+    }
 
 }
