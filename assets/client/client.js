6e68b77b6e84519b208350cf25d233d528096541
Anxium <axel.avx@gmail.com>
2019-02-26 10:29:49 +0100

    for debug

--- assets/client/client.js
+++ assets/client/client.js
@@ -2,7 +2,7 @@
 const config = {
     type: Phaser.AUTO,
     width: 1280,
-    height: 720,
+    height: 800,
     physics: {
         default: 'arcade',
         arcade: {
@@ -20,7 +20,7 @@ const game = new Phaser.Game(config)
 
 function preload() {
 
-    this.load.image("tiles", "img/tilesets/tiles_ju.png") 
+    this.load.image("tiles", "img/tilesets/tiles_ju.png")
 
     this.load.spritesheet('dude',
         'img/odul.png',
@@ -29,9 +29,10 @@ function preload() {
 
 }
 
-let player 
+let player
 
 const TILES = {
+    BLANK: 30,
     WALL: {
         TOP_LEFT: 0,
         TOP_RIGHT: 2,
@@ -56,72 +57,116 @@ const TILES = {
             [12],
             [13]
         ]
-    }
+    },
+    CHEST: [30],
+    STAIRS: [32],
+    OBSTACLE: [29]
 }
 
 function create() {
 
     this.dungeon = new Dungeon({
-        width: 50,
-        height: 50,
-        doorPadding: 5,
+        width: 80,
+        height: 80,
+        doorPadding: 4,
         rooms: {
-            width: { min: 12, max: 24 },
-            height: { min: 10, max: 16 },
-            maxRooms: 8
+            width: { min: 12, max: 24, onlyOdd: true },
+            height: { min: 10, max: 16, onlyOdd: true },
+            maxRooms: 3
         }
-    }) 
+    })
 
     // Creating a blank tilemap with dimensions matching the dungeon
     const map = this.make.tilemap({
-        tileWidth: 70,
-        tileHeight: 70,
+        tileWidth: 78,
+        tileHeight: 78,
         width: this.dungeon.width,
         height: this.dungeon.height
     })
-    const tileset = map.addTilesetImage("tiles", null, 70, 70, 1, 2)
+    const tileset = map.addTilesetImage("tiles", null, 78, 78, 1, 2)
     const groundLayer = map.createBlankDynamicLayer("Ground", tileset)
     const stuffLayer = map.createBlankDynamicLayer("Stuff", tileset)
 
     // Use the array of rooms generated to place tiles in the map
     // Note: using an arrow function here so that "this" still refers to our scene
     this.dungeon.rooms.forEach(room => {
-        const { x, y, width, height, left, right, top, bottom } = room 
+        const { x, y, width, height, left, right, top, bottom } = room
 
         // Fill the floor with mostly clean tiles
-        groundLayer.weightedRandomize(x + 1, y + 1, width - 2, height - 2, TILES.FLOOR) 
+        groundLayer.weightedRandomize(x + 1, y + 1, width - 2, height - 2, TILES.FLOOR)
 
         // Place the room corners tiles
-        groundLayer.putTileAt(TILES.WALL.TOP_LEFT, left, top) 
-        groundLayer.putTileAt(TILES.WALL.TOP_RIGHT, right, top) 
-        groundLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT, right, bottom) 
-        groundLayer.putTileAt(TILES.WALL.BOTTOM_LEFT, left, bottom) 
+        groundLayer.putTileAt(TILES.WALL.TOP_LEFT, left, top)
+        groundLayer.putTileAt(TILES.WALL.TOP_RIGHT, right, top)
+        groundLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT, right, bottom)
+        groundLayer.putTileAt(TILES.WALL.BOTTOM_LEFT, left, bottom)
 
         // Fill the walls with mostly clean tiles
-        groundLayer.weightedRandomize(left + 1, top, width - 2, 1, TILES.WALL.TOP) 
-        groundLayer.weightedRandomize(left + 1, bottom, width - 2, 1, TILES.WALL.BOTTOM) 
-        groundLayer.weightedRandomize(left, top + 1, 1, height - 2, TILES.WALL.LEFT) 
-        groundLayer.weightedRandomize(right, top + 1, 1, height - 2, TILES.WALL.RIGHT) 
+        groundLayer.weightedRandomize(left + 1, top, width - 2, 1, TILES.WALL.TOP)
+        groundLayer.weightedRandomize(left + 1, bottom, width - 2, 1, TILES.WALL.BOTTOM)
+        groundLayer.weightedRandomize(left, top + 1, 1, height - 2, TILES.WALL.LEFT)
+        groundLayer.weightedRandomize(right, top + 1, 1, height - 2, TILES.WALL.RIGHT)
 
         // Dungeons have rooms that are connected with doors. Each door has an x & y relative to the
         // room's location. Each direction has a different door to tile mapping.
         const doors = room.getDoorLocations()  // → Returns an array of {x, y} objects
         for (let i = 0; i < doors.length; i++) {
             if (doors[i].y === 0) {
-                groundLayer.putTilesAt(TILES.DOOR.TOP, x + doors[i].x - 1, y + doors[i].y) 
+                groundLayer.putTilesAt(TILES.DOOR.TOP, x + doors[i].x - 1, y + doors[i].y)
             } else if (doors[i].y === room.height - 1) {
-                groundLayer.putTilesAt(TILES.DOOR.BOTTOM, x + doors[i].x - 1, y + doors[i].y) 
+                groundLayer.putTilesAt(TILES.DOOR.BOTTOM, x + doors[i].x - 1, y + doors[i].y)
             } else if (doors[i].x === 0) {
-                groundLayer.putTilesAt(TILES.DOOR.LEFT, x + doors[i].x, y + doors[i].y - 1) 
+                groundLayer.putTilesAt(TILES.DOOR.LEFT, x + doors[i].x, y + doors[i].y - 1)
             } else if (doors[i].x === room.width - 1) {
-                groundLayer.putTilesAt(TILES.DOOR.RIGHT, x + doors[i].x, y + doors[i].y - 1) 
+                groundLayer.putTilesAt(TILES.DOOR.RIGHT, x + doors[i].x, y + doors[i].y - 1)
             }
         }
-    }) 
+    })
 
     // Not exactly correct for the tileset since there are more possible floor tiles, but this will
     // do for the example.
-    groundLayer.setCollisionByExclusion([12, 3, 4, 5, 6, 14, 15, 16, 17, 20, 18]) 
+    groundLayer.setCollisionByExclusion([12, 3, 4, 5, 6, 14, 15, 16, 17, 20, 18])
+
+    // Separate out the rooms into:
+    //  - The starting room (index = 0)
+    //  - A random room to be designated as the end room (with stairs and nothing else)
+    //  - An array of 90% of the remaining rooms, for placing random stuff (leaving 10% empty)
+    const rooms = this.dungeon.rooms.slice();
+    const startRoom = rooms.shift();
+    const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
+    const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
+
+    // Place the stairs
+    stuffLayer.putTileAt(TILES.STAIRS, endRoom.centerX, endRoom.centerY);
+
+    // Place stuff in the 90% "otherRooms"
+    otherRooms.forEach(room => {
+
+
+        stuffLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
+
+        // const rand = Math.random();
+        // if (rand <= 0.25) {
+        //     // 25% chance of chest
+        //     stuffLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
+        // } else if (rand <= 0.5) {
+        //     // 50% chance of a pot anywhere in the room... except don't block a door!
+        //     const x = Phaser.Math.Between(room.left + 2, room.right - 2);
+        //     const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
+        //     stuffLayer.weightedRandomize(x, y, 1, 1, TILES.OBSTACLE);
+        // } else {
+        //     // 25% of either 2 or 4 towers, depending on the room size
+        //     if (room.height >= 9) {
+        //         stuffLayer.putTilesAt(TILES.OBSTACLE, room.centerX - 2, room.centerY + 2);
+        //         stuffLayer.putTilesAt(TILES.OBSTACLE, room.centerX + 2, room.centerY + 2);
+        //         stuffLayer.putTilesAt(TILES.OBSTACLE, room.centerX - 2, room.centerY - 2);
+        //         stuffLayer.putTilesAt(TILES.OBSTACLE, room.centerX + 2, room.centerY - 2);
+        //     } else {
+        //         stuffLayer.putTilesAt(TILES.OBSTACLE, room.centerX - 2, room.centerY - 2);
+        //         stuffLayer.putTilesAt(TILES.OBSTACLE, room.centerX + 2, room.centerY - 2);
+        //     }
+        // }
+    });
 
     // Place the player in the center of the map. This works because the Dungeon generator places the first room in the center of the map.
     // 
@@ -148,11 +193,12 @@ function create() {
     })
 
     // Watch the player and layer for collisions, for the duration of the scene:
-    this.physics.add.collider(player, groundLayer) 
+    this.physics.add.collider(player, groundLayer)
+    this.physics.add.collider(player, stuffLayer)
 
     // Phaser supports multiple cameras, but you can access the default camera like this:
-    const camera = this.cameras.main 
-    camera.startFollow(player) 
+    const camera = this.cameras.main
+    camera.startFollow(player)
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 
 }
@@ -163,12 +209,12 @@ function update() {
     cursors = this.input.keyboard.createCursorKeys()
 
     if (cursors.left.isDown) {
-        player.setVelocityX(-160)
+        player.setVelocityX(-300)
 
         player.anims.play('left', true)
     }
     else if (cursors.right.isDown) {
-        player.setVelocityX(160)
+        player.setVelocityX(300)
 
         player.anims.play('right', true)
     }
@@ -179,10 +225,10 @@ function update() {
     }
 
     if (cursors.up.isDown) {
-        player.setVelocityY(-160)
+        player.setVelocityY(-300)
     }
     else if (cursors.down.isDown) {
-        player.setVelocityY(160)
+        player.setVelocityY(300)
     }
     else {
         player.setVelocityY(0)
